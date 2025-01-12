c6e5410540758d1847d31490dbc64d768b183210
Shrvin-git <shrvindavoodi@gmail.com>
2025-01-12 20:48:50 +0330

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -4,20 +4,31 @@ let destinationInputElem = document.querySelector('#destination')
 let inputPassenger = document.querySelector('#travelling')
 let arrowUpInput = document.querySelector('.sfa-angle-down')
 let replaceLocatian = document.querySelector('.replace-location')
-let faqElem = document.querySelectorAll('.accordion-btn')
-let faqPanel = document.querySelectorAll('.panel')
+let faqBoxes = document.querySelectorAll('.FAQ-box')
+let faqPanel = document.querySelectorAll('.FAQ-box_panel ')
 let footerAccordion = document.querySelector('.footer-accordion_clicable')
 let footerAccordionPanel = document.querySelector('.footer-accordion_panel')
 let footerAccordionIcon = document.querySelector('.footer-accordion__icon')
-
 let footerAccordionSuport = document.querySelector('.footer-suport-accordion_clicable')
 let footerAccordionSuportPanel = document.querySelector('.footer-suport-accordion_panel')
 let footerAccordionLogo = document.querySelector('.footer-suport-accordion__icon')
-faqElem.forEach(function (item) {
-    item.addEventListener('click', function (event) {
+let subMenu = document.querySelector('.sub-menu_mobile')
 
-        let descriptionFAQ = event.target.nextSibling.nextSibling
-        descriptionFAQ.classList.toggle('panel--toggle')
+
+faqBoxes.forEach(function (boxes) {
+    boxes.addEventListener('click', function (event) {
+
+        const isActive = boxes.classList.contains('faq-boxes_active')
+
+        faqBoxes.forEach(function (items) {
+            items.classList.remove('faq-boxes_active')
+        })
+
+        if (!isActive) {
+            boxes.classList.toggle('faq-boxes_active')
+        }
+
+
     })
 })
 inputPassenger.addEventListener('focus', function () {
@@ -50,6 +61,15 @@ footerAccordionSuport.addEventListener('click', function () {
     footerAccordionSuportPanel.classList.toggle('footer-suport-accordion_panel--toggle')
     footerAccordionLogo.classList.toggle('footer-suport-accordion__icon--toggle')
 })
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 62) {
+        subMenu.classList.add('sub-items_mobile-active')
+    }
+    else {
+        subMenu.classList.remove('sub-items_mobile-active')
+    }
+})
+
 
 
 
@@ -61,7 +81,7 @@ const swiper = new Swiper('.swiper', {
     slidesPerView: 'auto',
     spaceBetween: 30,
     freeMode: true,
-    // And if we need scrollbar
+
     scrollbar: {
         el: '.swiper-scrollbar',
     },
